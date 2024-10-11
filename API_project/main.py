dbdceefbd0ab35deaa83d5a04ca5ae0df29e5e1a
elian.casal <elian.casal@gmail.com>
2024-10-11 22:05:03 +0000

    Chaning to the python requests library

--- API_project/main.py
+++ API_project/main.py
@@ -1,16 +1,4 @@
-import creds
-api_key = creds.api_key
+import creds api_key = creds.api_key
 
-import http.client
-import json
 
-conn = http.client.HTTPSConnection("getpantry.cloud")
-payload = ''
-headers = {
-  'Content-Type': 'application/json'
-}
-conn.request("GET", f"/apiv1/pantry/{api_key}", payload, headers)
-res = conn.getresponse()
-data = res.read()
-print(data.decode("utf-8"))
 
--- API_project/put.py
+++ API_project/put.py
@@ -5,11 +5,14 @@ import json
 
 url = f"https://getpantry.cloud/apiv1/pantry/{api_key}"
 
-payload = ""
+payload = json.dumps({
+  "name": "Postman_Account_Updated",
+  "description": "Postman test account updated"
+})
 headers = {
   'Content-Type': 'application/json'
 }
 
-response = requests.request("GET", url, headers=headers, data=payload)
+response = requests.request("PUT", url, headers=headers, data=payload)
 
 print(response.text)
