cb490daf83c7ad187c14ef5e848a0dd06c52ef53
elian.casal <elian.casal@gmail.com>
2024-10-12 10:55:28 +0000

    moved .gitignore and made todo list.

--- API_project/main.py
+++ API_project/main.py
@@ -1,5 +1,22 @@
-import creds api_key = creds.api_key
+import creds 
+api_key = creds.api_key
 
 
-# todo set global variable for pantry name
+# TODO Create global variable for API key and pantry name ("pantry1")
+# TODO start out making some company json structures. what would they look like.
+# TODO make a call for a specific company and get the results.
+# TODO CLI
+# TODO make a bunch of documentation
+# 
 
+
+
+# 
+
+"""
+test
+"""
+
+#later considerations:
+
+#* START OF THE CLI CODE:
